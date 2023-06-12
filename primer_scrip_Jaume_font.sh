379972353ad8c9d51dbed37c4b2ccd334084bddc
Riu22 <riutordjaume@gmail.com>
2023-06-12 08:53:38 +0200

    primer_exercici_examen

---
+++ primer_scrip_Jaume_font.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+data=$(date)
+usuari=$USER
+echo "Benvinguts a l'examen de scripts, jo soc en/na ${usuari}. Avui és ${data}."
+
+nota_examen=$(( ( RANDOM % 10 )  + 1 ))
+echo "La nota del meu examen és ${nota_examen}"
+
+read -p "Has tigut una bona aptitud?: " resposta
+if [[ $resposta == "si" ]]; 
+then
+    nota_examen=$((nota_examen + 1))
+    
+elif [[ $resposta == "no" ]]; 
+then
+    nota_examen=$((nota_examen - 1))
+    
+fi
+
+
+if [[ ${nota_examen} -lt 4 ]]
+    then
+        echo "La teva nota final és un ${nota_examen}, estàs suspès."
+    else
+        echo "La teva nota final és un ${nota_examen}, estàs aprovat."
+fi
+
+
+
+
+
